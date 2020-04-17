27cdb664ac92b9f904e26f36ff40d7e46d267df6
Vicente <joao.vicenteft@gmail.com>
2020-04-17 16:24:51 -0400

    logistic regression in folders

--- neural-networks/logisticRegression/LR_cat_classification.py
+++ neural-networks/logisticRegression/LR_cat_classification.py
@@ -8,39 +8,19 @@ from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
 
-
-# Loading the data (cat/non-cat)
+# Load the data (cat/non-cat)
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
 index = 20
 plt.imshow(train_set_x_orig[index])
-
-#print the pictures of cats
-#print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
 
 
 m_train = train_set_x_orig.shape[0]
 m_test = test_set_x_orig.shape[0]
 num_px = train_set_x_orig.shape[1]
 
-print ("Number of training examples: m_train = " + str(m_train))
-print ("Number of testing examples: m_test = " + str(m_test))
-print ("Height/Width of each image: num_px = " + str(num_px))
-print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("train_set_x shape: " + str(train_set_x_orig.shape))
-print ("train_set_y shape: " + str(train_set_y.shape))
-print ("test_set_x shape: " + str(test_set_x_orig.shape))
-print ("test_set_y shape: " + str(test_set_y.shape))
-
 train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
 test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
-### END CODE HERE ###
-
-print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
-print ("train_set_y shape: " + str(train_set_y.shape))
-print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
-print ("test_set_y shape: " + str(test_set_y.shape))
-print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
 
 
 train_set_x = train_set_x_flatten / 255
@@ -61,7 +41,6 @@ def initialize_with_zeros(dim):
     assert(isinstance(b, float) or isinstance(b, int))
 
     return w, b
-
 
 def propagate(w, b, X, Y):
     
@@ -86,10 +65,6 @@ def propagate(w, b, X, Y):
 
 w, b, X, Y = np.array([[1], [2]]), 2, np.array([[1,2], [3,4]]), np.array([[1, 0]])
 grads, cost = propagate(w, b, X, Y)
-
-print ("dw = " + str(grads["dw"]))
-print ("db = " + str(grads["db"]))
-print ("cost = " + str(cost))
 
 
 def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
@@ -123,10 +98,6 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
 
 params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
 
-print ("w = " + str(params["w"]))
-print ("b = " + str(params["b"]))
-print ("dw = " + str(grads["dw"]))
-print ("db = " + str(grads["db"]))
 
 def predict(w, b, X):
     
@@ -134,7 +105,7 @@ def predict(w, b, X):
     Y_prediction = np.zeros((1, m))
     w = w.reshape(X.shape[0], 1)
     
-
+    
     A = sigmoid(np.dot(w.T, X) + b)
     
 
@@ -145,10 +116,6 @@ def predict(w, b, X):
     assert(Y_prediction.shape == (1, m))
     
     return Y_prediction
-
-
-
-print("predictions = " + str(predict(w, b, X)))
 
 def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
 
